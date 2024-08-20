emcangi/bluejay

commit 683bd7cb533cce29bb8517350794b2ec18362448

Overhaul the model to be generalized for any planet, selectable on run. - Simplify startup by creating the following files:   (1) INPUT_PARAMETERS.jl for user-defined inputs at runtime   (2) MODEL_SETUP.jl, which sets up variables and structures that depend upon INPUT_PARAMETERS, but don't need to be in the user's face   (3) PLOT_STYLES.jl, which allocates plot styling variables that will be called by both the Photochemistry module, the running model, and users later in analysis notebooks - Delete the following deprecated files: CUSTOMIZATIONS.jl, PARAMETERS.jl - Remove planetary parameters from CONSTANTS.jl and put them in MODEL_SETUP.jl - Move float type variables (ftype_ncur) to CONSTANTS.jl which is not ideal, but is the easiest thing to do since these variables are referenced by both the Photochemistry module and the running script - Move all Mars and Venus inputs to dedicated folders; rename the reaction network files appropriately - Update JOtoOpl.csv as done by Mike Chaffin for original iteration of the Venus model, when it was a separate repo - Update Photochemistry module files:   - Add planet, M_P (mass of planet), R_P (radius of planet) as passed-in global variables since they are no longer universal constants   - Add globvars... reference in any function calls which eventually contain a function that depends on the above new planetary parameters   - Add scaleH_lowerboundary to Atmosphere.jl to support Venus model's lower boundary at 90 km   - Update name of PARAMETERS.jl to INPUT_PARAMETERS.jl as called in get_paramfile()   - Add planet-based parameters for escape_probability()   - Add Venus-appropriate eddy diffusion   - Add capability to both log and later load altitude, and planet name, mass, and radius from PARAMETERS.xlsx   - Include monospace_choice and sansserif_choice font name variables in Plotting.jl   - Add ylims optional argument to plot_atm   - Update Temperature.jl to contain T_Mars and T_Venus   - Modify the water profile setup to do some

Photochemistry/src/Core.jl
@@ -2,10 +2,17 @@
 #                                                                              #
 #                             PHOTOCHEMICAL CORE                               #
 #                                                                              #
+# Everything necessary to run the model should be in this file with a few      #
+# exceptions:
+# 1. Anything related to file input/output/writing is in FileIO.jl.            #
+# 2. Everything related to photochemical cross sections is in Crosssections.jl.#
+# 3. Everything related to building the chemical network object from the       #
+#    provided spreadsheet is in ReactionNetwork.jl.
+# 
 # **************************************************************************** #
 
 #===============================================================================#
-#                        Basic model mechanical functions                       #
+#                      Atmospheric attribute calculations                       #
 #===============================================================================#
 
 function atm_dict_to_matrix(atmdict::Dict{Symbol, Vector{ftype_ncur}}, species_list)
@@ -632,12 +639,12 @@ function effusion_velocity(Texo, m; globvars...)
     =#
 
     GV = values(globvars)
-    required =  [:zmax]
+    required =  [:zmax, :M_P, :R_P]
     check_requirements(keys(GV), required)
     
     # lambda is the Jeans parameter (Gronoff 2020), basically the ratio of the 
     # escape velocity GmM/z to the thermal energy, kT.
-    lambda = (m*mH*bigG*marsM)/(kB*Texo*(radiusM+GV.zmax))
+    lambda = (m*mH*bigG*GV.M_P)/(kB*Texo*(GV.R_P+GV.zmax))
     vth = sqrt(2*kB*Texo/(m*mH))
     v = exp(-lambda)*vth*(lambda+1)/(2*pi^0.5)
 
@@ -656,13 +663,17 @@ function escape_probability(sp, atmdict; globvars...)::Array
         Array by altitude of escape probabilities for hot atoms. 0-1.
     =#
     GV = values(globvars)
-    required = [:all_species, :collision_xsect]
+    required = [:all_species, :collision_xsect, :dz, :planet]
     check_requirements(keys(GV), required)
     
-    A = 0.916 # escape probability at altitude where above column = 0, for high energy particles. upper limit
-    a = 0.039 # how "transparent" the atmosphere is to an escaping atom. smaller for higher energy so this is for an upper limit.
-    
-    return A .* exp.(-a .* GV.collision_xsect[sp] .* column_density_above(n_tot(atmdict; GV.all_species))) 
+    # Parameters determined through Bethan's Monte Carlo model. 
+    # [1] = A = escape probability at altitude where above column = 0, for high energy particles. upper limit
+    # [2] = a = how "transparent" the atmosphere is to an escaping atom. smaller for higher energy so this is for an upper limit.
+    params = Dict("Mars"=>[0.916, 0.39],
+                  "Venus"=>[0.868, 0.058]
+                 )[GV.planet]
+
+    return params[1] .* exp.(-params[2] .* GV.collision_xsect[sp] .* column_density_above(n_tot(atmdict; GV.all_species, GV.dz); globvars...) ) 
 end
 
 function escaping_hot_atom_production(sp, source_rxns, source_rxn_rc_funcs, atmdict, Mtot; returntype="array", globvars...)
@@ -1157,10 +1168,15 @@ function boundaryconditions(fluxcoef_dict, atmdict, M; nonthermal=true, globvars
         element in each row will eventually be multiplied by a density taken from the atmospheric 
         state dictionary, and the second element will be used as-is. That way, eventually the total
         change recorded in other functions is always #/cm³/s. 
+
+        FROM VENUS VERSION, MIKE:
+        Sign convention: The density-dependent terms (bc_dict[sp][:, 1]) are multiplied by -1 when the
+                         transport rates are computed in get_transport_PandL_rate. Density independent 
+                         terms (bc_dict[sp][:, 2]) are not.
     =#
     
     GV = values(globvars)
-    required = [:all_species, :speciesbclist, :dz]
+    required = [:all_species, :speciesbclist, :dz, :planet]
     check_requirements(keys(GV), required)
     
     bc_dict = Dict{Symbol, Array{ftype_ncur}}([s=>[0 0; 0 0] for s in GV.all_species])
@@ -1175,8 +1191,22 @@ function boundaryconditions(fluxcoef_dict, atmdict, M; nonthermal=true, globvars
  
         # DENSITY
         try 
-            n_lower = [fluxcoef_dict[sp][2, :][1], fluxcoef_dict[sp][1, :][2]*these_bcs["n"][1]]
+            # lower boundary...
+            if GV.planet=="Mars"
+                n_lower = [fluxcoef_dict[sp][2, :][1], fluxcoef_dict[sp][1, :][2]*these_bcs["n"][1]]
+            elseif GV.planet=="Venus"
+                # get the eddy+molecular mixing velocities at the lower boundary of the atmosphere
+                v_lower_boundary_up = fluxcoef_dict[sp][1, # lower boundary cell, outside atmosphere
+                                                        2] # upward mixing velocity
+                v_lower_boundary_dn = fluxcoef_dict[sp][2, # bottom cell of atmosphere
+                                                        1] # downward mixing velocity
+
+                n_lower = [v_lower_boundary_dn, v_lower_boundary_up*these_bcs["n"][1]]
+            end
             try
+                # TODO for Venus only, this note from Mike originally: 
+                # throw an error if density boundary condition
+                # is specified simultaneous with any flux or velocity condition
                 @assert all(x->!isnan(x), n_lower)
                 bc_dict[sp][1, :] .+= n_lower
             catch y
@@ -1184,8 +1214,25 @@ function boundaryconditions(fluxcoef_dict, atmdict, M; nonthermal=true, globvars
                     throw("Unhandled exception in lower density bc: $(y)")
                 end
             end
+
+            # upper boundary...
             try 
-                n_upper = [fluxcoef_dict[sp][end-1, :][2], fluxcoef_dict[sp][end, :][1]*these_bcs["n"][2]]
+                if GV.planet=="Mars"
+                    n_upper = [fluxcoef_dict[sp][end-1, :][2], fluxcoef_dict[sp][end, :][1]*these_bcs["n"][2]]
+                elseif GV.planet=="Venus"
+                    # get the eddy+molecular mixing velocities at the upper boundary of the atmosphere
+                    v_upper_boundary_up = fluxcoef_dict[sp][end-1, # top cell of atmosphere
+                                                            2]     # upward mixing velocity
+                    v_upper_boundary_dn = fluxcoef_dict[sp][end, # upper boundary cell, outside atmosphere
+                                                            1]   # downward mixing velocity
+
+                    n_upper = [v_upper_boundary_up, v_upper_boundary_dn*these_bcs["n"][2]]
+                    
+                end
+
+                # TODO for Venus only, this note from Mike originally: 
+                # throw an error if density boundary condition
+                # is specified simultaneous with any flux or velocity condition
                 @assert all(x->!isnan(x), n_upper)
                 bc_dict[sp][2, :] .+= n_upper
             catch y
@@ -1201,7 +1248,13 @@ function boundaryconditions(fluxcoef_dict, atmdict, M; nonthermal=true, globvars
     
         # FLUX 
         try 
-            f_lower = [0, -these_bcs["f"][1]/GV.dz]
+            # lower boundary...
+            if GV.planet=="Mars"
+                f_lower = [0, -these_bcs["f"][1]/GV.dz]
+            elseif GV.planet=="Venus"
+                f_lower = [0, these_bcs["f"][1]/GV.dz]
+                #             ^ no (-) sign, negative flux at lower boundary represents loss to surface
+            end
             try        
                 @assert all(x->!isnan(x), f_lower)
                 bc_dict[sp][1, :] .+= f_lower
@@ -1211,7 +1264,10 @@ function boundaryconditions(fluxcoef_dict, atmdict, M; nonthermal=true, globvars
                 end
             end
             try 
+                # upper boundary...
                 f_upper = [0, -these_bcs["f"][2]/GV.dz]
+                #             ^ (-) sign needed so that positive flux at upper boundary represents loss to space
+                #             (see "Sign convention" note above)
                 @assert all(x->!isnan(x), f_upper)
                 bc_dict[sp][2, :] .+= f_upper
             catch y
@@ -1227,7 +1283,14 @@ function boundaryconditions(fluxcoef_dict, atmdict, M; nonthermal=true, globvars
     
         # VELOCITY
         try 
-            v_lower = [these_bcs["v"][1]/GV.dz, 0]
+            # lower boundary...
+            if GV.planet=="Mars" 
+                v_lower = [these_bcs["v"][1]/GV.dz, 0]
+            elseif GV.planet=="Venus"
+                v_lower = [-these_bcs["v"][1]/GV.dz, 0]
+            #          ^ (-) sign needed so that negative velocity at lower boundary represents loss to surface
+            #          (see "Sign convention" note above)
+            end
 
             try
                 @assert all(x->!isnan(x), v_lower)
@@ -1239,7 +1302,10 @@ function boundaryconditions(fluxcoef_dict, atmdict, M; nonthermal=true, globvars
             end
 
             try 
+                # upper boundary...
                 v_upper = [these_bcs["v"][2]/GV.dz, 0]
+                #          ^ no (-) sign needed,  positive velocity at upper boundary represents loss to space
+                #          (see "Sign convention" note above)
                 @assert all(x->!isnan(x), v_upper)
                 bc_dict[sp][2, :] .+= v_upper
             catch y
@@ -1430,7 +1496,7 @@ function fluxcoefs(sp::Symbol, Kv, Dv, H0v; globvars...)
     =#
 
     GV = values(globvars)
-    required = [:Tn, :Tp, :Hs_dict, :n_all_layers, :dz]
+    required = [:Tn, :Tp, :Hs_dict, :n_all_layers, :dz, :planet]
     check_requirements(keys(GV), required)
 
     # Initialize arrays for downward (i to i-1) and upward (i to i+1) coefficients
@@ -1462,17 +1528,30 @@ function fluxcoefs(sp::Symbol, Kv, Dv, H0v; globvars...)
     Hsl[2:end] = @. (GV.Hs_dict[sp][1:end-1] + GV.Hs_dict[sp][2:end]) / 2.0
     H0l[2:end] = @. (H0v[charge_type(sp)][1:end-1] + H0v[charge_type(sp)][2:end]) / 2.0
 
-    # Handle the lower boundary layer:
-    Dl[1] = @. (1 + Dv[sp][1]) /  2.0
-    Kl[1] = @. (1 + Kv[1]) / 2.0
-    Tl_n[1] = @. (1 + GV.Tn[1]) / 2.0
-    Tl_p[1] = @. (1 + GV.Tp[1]) / 2.0
-    dTdzl_n[1] = @. (GV.Tn[1] - 1) / GV.dz
-    dTdzl_p[1] = @. (GV.Tp[1] - 1) / GV.dz
-    Hsl[1] = @. (1 + GV.Hs_dict[sp][1]) / 2.0
-    H0l[1] = @. (1 + H0v[charge_type(sp)][1]) / 2.0
+    if GV.planet=="Mars"
+        # Handle the lower boundary layer:
+        Dl[1] = @. (1 + Dv[sp][1]) /  2.0
+        Kl[1] = @. (1 + Kv[1]) / 2.0
+        Tl_n[1] = @. (1 + GV.Tn[1]) / 2.0
+        Tl_p[1] = @. (1 + GV.Tp[1]) / 2.0
+        dTdzl_n[1] = @. (GV.Tn[1] - 1) / GV.dz
+        dTdzl_p[1] = @. (GV.Tp[1] - 1) / GV.dz
+        Hsl[1] = @. (1 + GV.Hs_dict[sp][1]) / 2.0
+        H0l[1] = @. (1 + H0v[charge_type(sp)][1]) / 2.0
+    elseif GV.planet=="Venus"
+        # Downward transport away from the lower boundary layer, which is outside the model
+        # These should never be used but we need to fill the array
+        Dl[1] = Float64(NaN)
+        Kl[1] = Float64(NaN)
+        Tl_n[1] = Float64(NaN)
+        Tl_p[1] = Float64(NaN)
+        dTdzl_n[1] = Float64(NaN)
+        dTdzl_p[1] = Float64(NaN)
+        Hsl[1] = Float64(NaN)
+        H0l[1] = Float64(NaN)
+    end
 
-    # Now the coefficients between this layer and upper layer
+    # Upward transport from each altitude to the cell above
     Du[1:end-1] = @. (Dv[sp][1:end-1] + Dv[sp][2:end]) /  2.0
     Ku[1:end-1] = @. (Kv[1:end-1] + Kv[2:end]) / 2.0
     Tu_n[1:end-1] = @. (GV.Tn[1:end-1] + GV.Tn[2:end]) / 2.0
@@ -1482,15 +1561,28 @@ function fluxcoefs(sp::Symbol, Kv, Dv, H0v; globvars...)
     Hsu[1:end-1] = @. (GV.Hs_dict[sp][1:end-1] + GV.Hs_dict[sp][2:end]) / 2.0
     H0u[1:end-1] = @. (H0v[charge_type(sp)][1:end-1] + H0v[charge_type(sp)][2:end]) / 2.0
 
-    # Handle upper boundary layer:
-    Du[end] = @. (Dv[sp][end] + 1) /  2.0
-    Ku[end] = @. (Kv[end] + 1) / 2.0
-    Tu_n[end] = @. (GV.Tn[end] + 1) / 2.0
-    Tu_p[end] = @. (GV.Tp[end] + 1) / 2.0
-    dTdzu_n[end] = @. (1 - GV.Tn[end]) / GV.dz
-    dTdzu_p[end] = @. (1 - GV.Tp[end]) / GV.dz
-    Hsu[end] = @. (GV.Hs_dict[sp][end] + 1) / 2.0
-    H0u[end] = @. (H0v[charge_type(sp)][end] + 1) / 2.0
+    if GV.planet=="Mars"
+        # Handle upper boundary layer:
+        Du[end] = @. (Dv[sp][end] + 1) /  2.0
+        Ku[end] = @. (Kv[end] + 1) / 2.0
+        Tu_n[end] = @. (GV.Tn[end] + 1) / 2.0
+        Tu_p[end] = @. (GV.Tp[end] + 1) / 2.0
+        dTdzu_n[end] = @. (1 - GV.Tn[end]) / GV.dz
+        dTdzu_p[end] = @. (1 - GV.Tp[end]) / GV.dz
+        Hsu[end] = @. (GV.Hs_dict[sp][end] + 1) / 2.0
+        H0u[end] = @. (H0v[charge_type(sp)][end] + 1) / 2.0
+    elseif GV.planet=="Venus"
+        # Upwards flux from the upper boundary layer, which is outside the model
+        # These should never be used but we need to fill the array
+        Du[end] = Float64(NaN)
+        Ku[end] = Float64(NaN)
+        Tu_n[end] = Float64(NaN)
+        Tu_p[end] = Float64(NaN)
+        dTdzu_n[end] = Float64(NaN)
+        dTdzu_p[end] = Float64(NaN)
+        Hsu[end] = Float64(NaN)
+        H0u[end] = Float64(NaN)
+    end
 
 
     # two flux terms: eddy diffusion and gravity/thermal diffusion.
@@ -1569,7 +1661,7 @@ function fluxcoefs(species_list::Vector, K, D, H0; globvars...)
     return fluxcoef_dict
 end
 
-function Keddy(z::Vector, nt::Vector)
+function Keddy(z::Vector, nt::Vector; globvars...)
     #=
     Input:
         z: Altitude in cm
@@ -1578,10 +1670,18 @@ function Keddy(z::Vector, nt::Vector)
         k: eddy diffusion coefficients at all altitudes.
     =#
 
-    k = zeros(size(z))
-    upperatm = findall(i->i .> 60e5, z)
-    k[findall(i->i .<= 60e5, z)] .= 10. ^ 6
-    k[upperatm] .= 2e13 ./ sqrt.(nt[upperatm])
+    GV = values(globvars)
+    required = [:planet]
+    check_requirements(keys(GV), required)
+
+    k = zeros(size(z)) # Initialize array for eddy diffusion
+    if GV.planet=="Mars"
+        upperatm = findall(i->i .> 60e5, z)
+        k[findall(i->i .<= 60e5, z)] .= 10. ^ 6
+        k[upperatm] .= 2e13 ./ sqrt.(nt[upperatm])
+    elseif GV.planet=="Venus"
+        k = 8e12*(nt .^ -0.5)
+    end
 
     return k
 end
@@ -1609,13 +1709,13 @@ function update_diffusion_and_scaleH(species_list, atmdict::Dict{Symbol, Vector{
         H0: Dictionary of mean atmospheric scale height by altitude. Keys are "neutral" and "ion". 
     =#
     GV = values(globvars)
-    required = [:all_species, :alt, :speciesbclist, :molmass, :neutral_species, :n_alt_index, :polarizability, :q,
+    required = [:all_species, :alt, :speciesbclist, :M_P, :molmass, :neutral_species, :n_alt_index, :polarizability, :planet, :R_P, :q,
                :Tn, :Tp, :Tprof_for_diffusion, :use_ambipolar, :use_molec_diff]
     check_requirements(keys(GV), required)
 
     ncur_with_bdys = ncur_with_boundary_layers(atmdict; GV.n_alt_index, GV.all_species)
     
-    K = Keddy(GV.alt, n_tot(ncur_with_bdys; GV.all_species, GV.n_alt_index))
+    K = Keddy(GV.alt, n_tot(ncur_with_bdys; GV.all_species, GV.n_alt_index); GV.planet)
     H0_dict = Dict{String, Vector{ftype_ncur}}("neutral"=>scaleH(ncur_with_bdys, GV.Tn; globvars...),
                                                "ion"=>scaleH(ncur_with_bdys, GV.Tp; globvars...))
     
@@ -1650,9 +1750,9 @@ function update_transport_coefficients(species_list, atmdict::Dict{Symbol, Vecto
 
     GV = values(globvars)
     required = [:all_species, :alt, :speciesbclist, :dz, :hot_H_network, :hot_H_rc_funcs, :hot_D_network, :hot_D_rc_funcs, 
-               :hot_H2_network, :hot_H2_rc_funcs, :hot_HD_network, :hot_HD_rc_funcs,  
-               :Hs_dict, :ion_species, :molmass, :neutral_species, :non_bdy_layers, :num_layers, :n_all_layers, :n_alt_index, 
-               :polarizability, :q, :Tn, :Ti, :Te, :Tp, :Tprof_for_diffusion, :transport_species, :use_ambipolar, :use_molec_diff, :zmax]
+               :hot_H2_network, :hot_H2_rc_funcs, :hot_HD_network, :hot_HD_rc_funcs, :Hs_dict, 
+               :ion_species, :M_P, :molmass, :neutral_species, :non_bdy_layers, :num_layers, :n_all_layers, :n_alt_index, 
+               :polarizability, :q, :R_P, :Tn, :Ti, :Te, :Tp, :Tprof_for_diffusion, :transport_species, :use_ambipolar, :use_molec_diff, :zmax]
     check_requirements(keys(GV), required)
     
     # Update the diffusion coefficients and scale heights

CONSTANTS.jl
@@ -5,24 +5,19 @@
 # 
 # Eryn Cangi
 # Created November 2021
-# Currently tested for Julia: 1.6.1
+# Currently tested for Julia: 1.8.5
 ################################################################################
 
+#                              Universal constants
+# ===============================================================================
 const kB_MKS = 1.38e-23;        # J/K - needed for saturation vapor pressure empirical equation.
 const kB = 1.38e-16;            # erg/K
 const bigG = 6.67e-8;           # dyne-cm^2/g^2
 const mH = 1.67e-24;            # g 
-const marsM = 0.1075*5.972e27;  # g 
-const radiusM = 3396e5;         # cm
-const g = bigG * marsM / (radiusM^2);
 const q = -4.8032e-10            # statcoulomb (cm^1.5 g^0.5 s^-1)
-const DH = 5.5 * 1.6e-4         # SMOW value, Yung 1988
-const density_water_cgs = 0.997408
-const SA_Mars = 4*pi*(radiusM)^2 # cm^2
-
-#time 
-const sol_in_sec = 88775.2438   # One Mars sol in seconds
+const density_water_cgs = 0.997408  # used mostly for conversions
 const s_per_yr = 3.154e7
+
 
 # Polarizability from NIST. Experimental values from: https://cccbdb.nist.gov/pollistx.asp
 # Calculations for species not available in experiment from: https://cccbdb.nist.gov/polcalc2x.asp
@@ -90,3 +85,18 @@ const collision_xsect = Dict(:H=>4e-15, # Zhang 2009
                              :HD=>5e-15 # assumption that adding a proton or neutron adds 0.5e-15 to the cross section...
                             ) # Units of cm^2; Bohr radius 8.79e-17
 
+
+#                                 Float types
+# ===============================================================================
+# This section was introduced by Mike to attempt to solve issues with convergence. 
+# However, the model is now running without use of doubles, so this may be removable,
+# but has been left just in case it's ever needed again. Use of Doubles increases
+# model run time but potentially delivers higher stability in complex simulations. 
+# This is probably NOT the best place for it, but is the minimally annoying thing to 
+# do after a major reorganization April 2024 by Eryn. This way it can be shared between
+# the photochemistry module and the converge_new_file.jl code. 
+
+ftype_ncur = Float64  # used to store n_current values
+    # OPTIONS: Float64, Double64   
+ftype_chem = Float64 #Double64 #  used to compute chemical reaction rates and chemical jacobian
+    # OPTIONS: Float64, Double64   

CUSTOMIZATIONS.jl
@@ -1,136 +1,0 @@
-################################################################################
-# CUSTOMIZATIONS.jl
-# TYPE: (1) Model files - required
-# DESCRIPTION: Inputs for functions in the photochemistry module that could
-# ostensibly change or need to be called from outside the module. Includes,
-# for instance, the names of files containing photochemical cross sections,
-# colors to use for species density plots, and so forth.
-#
-# Eryn Cangi
-# Updated: January 2022
-# Currently tested for Julia: 1.6.1
-################################################################################
-
-using DoubleFloats
-
-# **************************************************************************** #
-#                                                                              #
-#                       Pathnames and folder locations                         #
-#                                                                              #
-# **************************************************************************** #
-
-const code_dir = "$(@__DIR__)/"
-const results_dir = code_dir*"../Results-Paper3/"
-const xsecfolder = code_dir*"uvxsect/";
-
-# **************************************************************************** #
-#                                                                              #
-#                                 Float types                                  #
-#                                                                              #
-# **************************************************************************** #
-# needed by both Photochemistry.jl and converge_new_file so it has to go here
-# This can be changed to Double64 to improve precision of calculations, but usually shouldn't be needed
-ftype_ncur = Float64 #Double64 #  used to store n_current values
-ftype_chem = Float64 #Double64 #  used to compute chemical reaction rates and chemical jacobian
-
-# **************************************************************************** #
-#                                                                              #
-#                  Altitude specifications - rarely changed                    #
-#                                                                              #
-# **************************************************************************** #
-const max_alt = 250e5
-const dz = 2e5
-const alt = convert(Array, (0:dz:max_alt))
-const n_all_layers = length(alt)
-const intaltgrid = round.(Int64, alt/1e5)[2:end-1]; # the altitude grid CELLS but in integers.
-const non_bdy_layers = alt[2:end-1]  # all layers, centered on 2 km, 4...248. Excludes the boundary layers which are [-1, 1] and [249, 251].
-const num_layers = length(non_bdy_layers) # there are 124 non-boundary layers.
-const plot_grid = non_bdy_layers ./ 1e5;  # for plotting. Points located at atmospheric layer cell centers and in units of km.
-
-const zmin = alt[1]
-const zmax = alt[end];
-const n_alt_index=Dict([z=>clamp((i-1),1, num_layers) for (i, z) in enumerate(alt)])
-
-const hygropause_alt = 40e5
-
-# **************************************************************************** #
-#                                                                              #
-#                                   STYLE                                      #
-#                                                                              #
-# **************************************************************************** #
-
-# Sans-serif font to use 
-sansserif_choice = "Louis George Caf?"
-
-# Monospace font to use
-monospace_choice = "FreeMono"
-
-const speciescolor = Dict( # PRIMARY NEUTRALS + IONS
-                    :CO2 =>"#000000", :CO2pl=>"#000000",
-                    :CO =>"#ff6600", :COpl=>"#ff6600",
-                    :N2=>"#aaaaaa", :N2pl=>"#aaaaaa", :Nup2D=>"#aaa",
-                    :Ar=>"#808080", :Arpl=>"#808080", :ArHpl=>"#956979", :ArDpl=>"#956979",
-                    :HCO=> "#33bbf9", :HCOpl=>"#33bbf9", :DCO=> "#33bbf9", :DCOpl=>"#33bbf9",
-                    :HOCpl=>"#c71a85", :DOCpl=>"#c71a85",
-                    :HOCO =>"#667522", :DOCO =>"#667522", :HCO2pl=>"#667522", :DCO2pl=>"#667522",
-                    :O1D =>"#7c3b6b",
-                    :O => "#7922b4", :Opl=>"#7922b4",
-                    :O2 => "#1BAD53", :O2pl=>"#1BAD53",
-                    :O3 =>"#1c4bb4",
-
-                    # ODD HYDROGEN + IONS
-                    :H => "#ed3e7e", :Hpl=>"#ed3e7e", :D => "#ed3e7e", :Dpl=>"#ed3e7e", 
-                    :H2 => "#964550", :H2pl=>"#964550", :HD => "#964550", :HDpl=>"#964550", 
-                    :H3pl=> "#A81047", :H2Dpl=>"#A81047", # :HD2pl=>"#A81047",
-                    :H2O => "#0258ff", :H2Opl=>"#0258ff", :HDO => "#0258ff", :HDOpl=>"#0258ff", 
-                    :H3Opl=> "#4f2381", :H2DOpl=>"#4f2381", 
-                    :H2O2 =>"#d48f4d", :HDO2 =>"#d48f4d", 
-                    :HO2 => "#609111", :HO2pl=>"#609111", :DO2 => "#609111",    
-                    
-                    :OH => "#a968d2", :OHpl=>"#a968d2", :OD => "#a968d2", :ODpl=>"#a968d2", 
-
-
-                    # NITROGEN NEUTRALS + IONS
-                    :C=>"#d9c382",:Cpl=>"#d9c382",
-                    :CH=>"#cea3ce",:CHpl=>"#cea3ce",
-                    :CN=>"#6d5000",:CNpl=>"#6d5000",
-                    :HCN=>"#519169",:HCNpl=>"#519169",     
-                    #:HCNHpl=>"#1d3971",
-                    :HNO=>"#ff1c5d",:HNOpl=>"#ff1c5d",
-                    #:HN2Opl=>"#fb7562",
-                    :N=>"#6e748a",:Npl=>"#6e748a",
-                    :N2O=>"#ca9260",:N2Opl=>"#ca9260",
-                    :NH=>"#059dc5",:NHpl=>"#059dc5", 
-                    #:NH2=>"#0b522e",:NH2pl=>"#0b522e", 
-                    #:NH3pl=>"#4ba40b", 
-                    :NO=>"#e639b1",:NOpl=>"#e639b1",
-                    :NO2=>"#a492e5", :NO2pl=>"#a492e5",  
-                    :N2Hpl=>"#611115",:N2Dpl=>"#611115", 
-                    );
-
-const medgray = "#444444"
-
-# **************************************************************************** #
-#                                                                              #
-#                       Crosssection filenames                                 #
-#                                                                              #
-# **************************************************************************** #
-
-
-const photochem_data_files = Dict(:CO2=>Dict("main"=>"CO2.dat"), 
-                                   :H2O=>Dict("main"=>"h2oavgtbl.dat"), 
-                                   :HDO=>Dict("main"=>"HDO.dat"), #"h2oavgtbl.dat"),
-                                   :H2O2=>Dict("main"=>"H2O2.dat"), 
-                                   :HDO2=>Dict("main"=>"H2O2.dat"), 
-                                   :O3=>Dict("main"=>"O3.dat", "chapman"=>"O3Chap.dat"), 
-                                   :O2=>Dict("main"=>"O2.dat", "schr_short"=>"130-190.cf4", "schr_mid"=>"190-280.cf4", "schr_long"=>"280-500.cf4"), 
-                                   :H2=>Dict("main"=>"binnedH2.csv"), 
-                                   :HD=>Dict("main"=>"binnedH2.csv"), 
-                                   :OH=>Dict("main"=>"binnedOH.csv", "O1D+H"=>"binnedOHo1D.csv"), 
-                                   :OD=>Dict("main"=>"OD.csv"))
-
-if photochem_data_files[:HDO]["main"] == "h2oavgtbl.dat" 
-    for i in 1:10
-        println("WARNING: HDO CROSS SECTIONS = H2O CROSS SECTIONS! DON'T FORGET TO CHANGE THIS BACK AFTER FINISHING THE RUN!")
-    end
-end

commit 492c4d87e1195d32536eae1c7f62cee632dcc480

Fix a typo introduced in 683bd7c which caused a major bug in non-thermal escape for Mars; somehow, the power on the exponential was transcribed while generalizing the model as 0.39 instead of 0.039. I don't know how I managed to run my notebooks checking that I hadn't ruined the results without noticing this error but somehow I did. The same bug was not introduced in the Venus values of the parameter so no change has been made there. For future reference, the correct value for Mars is found in Gregory+2023b, JGR:P, Section 3.2, equation 16.

Photochemistry/src/Core.jl
@@ -1049,7 +1049,7 @@ function escape_probability(sp, atmdict; globvars...)::Array
     # Parameters determined through Bethan's Monte Carlo model. 
     # [1] = A = escape probability at altitude where above column = 0, for high energy particles. upper limit
     # [2] = a = how "transparent" the atmosphere is to an escaping atom. smaller for higher energy so this is for an upper limit.
-    params = Dict("Mars"=>[0.916, 0.39],
+    params = Dict("Mars"=>[0.916, 0.039],
                   "Venus"=>[0.868, 0.058]
                  )[GV.planet]
 