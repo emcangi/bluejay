emcangi/bluejay

commit b41e971b3b57515c5ea6e3c6663603eedb3788b9

Update the way species are handled so that each planet has its own species list, and that the lists of species already in the model and the lists of new species are in INPUT_PARAMETERS, where they are more visible to the user. Clean up and remove some unnecessary variables (remove_ignored_species, ignored_species). Clarify what each species list variable means with some substantial comments and docstrings.

MODEL_SETUP.jl
@@ -6,7 +6,7 @@
 # 
 # Eryn Cangi
 # Created April 2024
-# Last edited: May 2024
+# Last edited: August 2024
 # Currently tested for Julia: 1.8.5
 ################################################################################
 
@@ -56,51 +56,21 @@ const SA = 4*pi*(R_P)^2 # cm^2
 #                                                                                                       #
 # ***************************************************************************************************** #
 
-remove_ignored_species = true # Whether to use a slightly smaller list of species and reactions (removing minor species that Roger had in his model)
-ignored_species = [:CNpl,:HCNpl,:HCNHpl,:HN2Opl,:NH2pl,:NH3pl,:CH,:CN,:HCN,:HNO,:NH,:NH2,:HD2pl]#
-
-#                                        Neutrals
-# =======================================================================================================
-const orig_neutrals = [:Ar, :CO, :CO2, :H, :H2, :H2O, :H2O2, 
-                       :HO2, :HOCO, :N2, 
-                       :O, :O1D, :O2, :O3, :OH,
-                       :D, :DO2, :DOCO, :HD, :HDO, :HDO2, :OD,
-                        :Cl, :ClO, :HCl, :ClCO, :DCl,
-                        :S, :SO, :SO2, :SO3, :H2SO4, :HDSO4,
-                        :N2O, :NO2,
-                       # Turn these off for minimal ionosphere:
-                       :C, :DCO, :HCN, :HCO, :N, :NO, :Nup2D, :N2O, :NO2
-                       ]; 
-const conv_neutrals = remove_ignored_species==true ? setdiff(orig_neutrals, ignored_species) : orig_neutrals
-const new_neutrals = [];
-const neutral_species = [conv_neutrals..., new_neutrals...];
-
-#                                          Ions
-# =======================================================================================================
-const orig_ions = [:CO2pl, :HCO2pl, :Opl, :O2pl, # Nair minimal ionosphere 
-                   :Arpl, :ArHpl, :ArDpl, 
-                   :Cpl, :CHpl,  :COpl, 
-                   :Hpl, :Dpl, :H2pl, :HDpl, :H3pl, :H2Dpl, :HD2pl, 
-                   :H2Opl,  :HDOpl, :H3Opl, :H2DOpl, 
-                   :HO2pl, :HCOpl, :DCOpl, :HOCpl, :DOCpl, :DCO2pl, 
-                   :HNOpl,   
-                   :Npl, :NHpl, :N2pl, :N2Hpl, :N2Dpl, :NOpl, :N2Opl, :NO2pl,
-                   :OHpl, :ODpl];
-const new_ions = [];
-const ion_species = remove_ignored_species==true ? setdiff([orig_ions..., new_ions...], ignored_species) : [orig_ions..., new_ions...]
-const nontherm = ions_included==true ? true : false   # whether to do non-thermal escape; this has to be here because it's needed in short order to do Jrates.
+# Minor species that have reactions available in the network files, but aren't used. These are just for
+# reference: [:CNpl,:HCNpl,:HCNHpl,:HN2Opl,:NH2pl,:NH3pl,:CH,:CN,:HCN,:HNO,:NH,:NH2,:HD2pl]
  
 #                                     Full species list
 # =======================================================================================================
+const neutral_species = [conv_neutrals[planet]..., new_neutrals...];
+const ion_species = [conv_ions[planet]..., new_ions...]
 const new_species = [new_neutrals..., new_ions...]  # Needed later to be excluded from n_tot() as called 
                                                     # in the water saturation calculation, in the case
                                                     # where new species are being added.
 const all_species = [neutral_species..., ion_species...];
 
-
 #                        Photolysis and Photoionization rate symbol lists 
 # =======================================================================================================
-
+const nontherm = ions_included==true ? true : false   # whether to do non-thermal escape. Must be here, sued in call to format Jrates
 const conv_Jrates, newJrates = format_Jrates(reaction_network_spreadsheet, all_species, "Jratelist"; ions_on=ions_included, hot_atoms=nontherm)
 const Jratelist = [conv_Jrates..., newJrates...];
 

INPUT_PARAMETERS.jl
@@ -1,10 +1,14 @@
 ################################################################################
 # INPUT_PARAMETERS.jl
-# DESCRIPTION: Variables to be modified by the user for each simulation run. 
+# DESCRIPTION: Controls key variables that set up the model, usually modified at 
+# each run by the user. Planet to model, logging notes, filenames, whether it's 
+# a seasonal cycle or equilibrium run, which atmospheric parameter to vary, 
+# solar input, water parameters, species modeled, algorithm tolerances, and stuff
+# to control which species densities are updated due to chemistry or transport.
 # 
 # Eryn Cangi
 # Created April 2024
-# Last edited: May 2024
+# Last edited: August 2024
 # Currently tested for Julia: 1.8.5
 ################################################################################
 
@@ -92,7 +96,67 @@ const H2O_excess = 250  # excess H2O in ppm
 const HDO_excess = 0.350 # excess HDO in ppm (divide by 1000 to get ppb)
     # OPTIONS: 0.350 is reasonable
 
-# Settings to control species in the model
+
+# Control which species (atoms, molecules) are modeled 
+# =======================================================================================================
+
+# Species lists
+# -------------------------------------------------------------------
+# Convention: Alphabetized, except D-bearing species should be mixed in after their H-bearing isotopologue. 
+# this makes it easier to see which species have isotopologues.
+# 
+# WHAT DO THESE LISTS MEAN? 
+# 
+# new_[neutrals, ions]: These are species you want to add to the modeled atmosphere on THIS RUN.
+#                       They should not have a density vector in the initial guess file.
+#                       Their initial density guess will be either all 0, or loaded from a file 
+#                       depending on what you specify for the use_nonzero_initial_profiles variable.
+#                       Note that after you complete a successful run with new species,
+#                       you must modify this file manually and enter those species into the 
+#                       conv_[neutrals,ions] variable. No way around it!
+#
+# conv_[neutrals,ions]: These are species which have already been incorporated into the model atmosphere.
+#                       They should already have a density vector in your initial guess file. 
+#                       Typically you won't need to change these at each run, UNLESS you added new species 
+#                       on the previous run.
+const new_neutrals = [];
+const new_ions = [];
+
+const conv_neutrals = Dict("Mars"=>[:Ar, :C, :CO, :CO2, # Argon and carbon species
+                                    :H, :D, :H2, :HD, :H2O, :HDO,  # H and D species
+                                    :HCO, :DCO, :HO2, :DO2,        
+                                    :H2O2, :HDO2, :HOCO, :DOCO, 
+                                    :N, :N2, :NO, :Nup2D, :N2O, :NO2, # Nitrogen species
+                                    :O, :O1D, :O2, :O3, :OH, :OD], # Oxygen species
+                           "Venus"=>[:Ar, :C, :CO, :CO2, 
+                                     :Cl, :ClO, :ClCO, :HCl, :DCl,  # Chlorine species
+                                     :H, :D, :H2, :HD, :H2O, :HDO,  # H and D species
+                                     :HCO, :DCO, :HO2, :DO2,        
+                                     :H2O2, :HDO2, :HOCO, :DOCO, 
+                                     :N, :N2, :NO, :Nup2D, :N2O, :NO2,
+                                     :O, :O1D, :O2, :O3, :OH, :OD,
+                                     :S, :SO, :SO2, :SO3, :H2SO4, :HDSO4] # Sulfur species
+                           ); 
+
+const conv_ions = Dict("Mars"=>[:Arpl, :ArHpl, :ArDpl, 
+                                :Cpl, :CHpl, :COpl, :CO2pl, 
+                                :Dpl, :DCOpl, :DOCpl, :DCO2pl, 
+                                :Hpl,  :H2pl, :HDpl, :H3pl, :H2Dpl, 
+                                :H2Opl, :HDOpl, :H3Opl, :H2DOpl, 
+                                :HO2pl, :HCOpl, :HCO2pl, :HOCpl, :HNOpl,   
+                                :Npl, :NHpl, :N2pl, :N2Hpl, :N2Dpl, :NOpl, :N2Opl, :NO2pl,
+                                :Opl, :O2pl, :OHpl, :ODpl],
+                       "Venus"=>[:Arpl, :ArHpl, :ArDpl, 
+                                :Cpl, :CHpl, :COpl, :CO2pl, 
+                                :Dpl, :DCOpl, :DOCpl, :DCO2pl, 
+                                :Hpl,  :H2pl, :HDpl, :H3pl, :H2Dpl, 
+                                :H2Opl, :HDOpl, :H3Opl, :H2DOpl, 
+                                :HO2pl, :HCOpl, :HCO2pl, :HOCpl, :HNOpl,   
+                                :Npl, :NHpl, :N2pl, :N2Hpl, :N2Dpl, :NOpl, :N2Opl, :NO2pl,
+                                :Opl, :O2pl, :OHpl, :ODpl]
+                      );
+
+# More specific settings for controling the modeling of species
 # -------------------------------------------------------------------
 const ions_included = true
 const converge_which = "both"

commit bd35c0a342f231fff4c4c3e735af9c2076200d74

Fix a bug introduced in b41e971b3b57515c5ea6e3c6663603eedb3788b9 where convergence won't work if only neutrals or only ions are being converged, because conv_ions and conv_neutrals were missing the [planet] key index. Also, move the addition of species from dont_compute_either_chem_or_transport to no_chem_species and no_transport_species to before the scenarios where they are only added to one or the other, for better logical flow.

MODEL_SETUP.jl
@@ -109,6 +109,11 @@ no_transport_species = [];
 
 # Fixed species (Densities don't update)
 # -------------------------------------------------------------------
+for s in dont_compute_either_chem_or_transport
+    push!(no_chem_species, s)
+    push!(no_transport_species, s)
+end
+
 for s in dont_compute_chemistry
     if ~(s in no_chem_species)
         push!(no_chem_species, s)
@@ -120,19 +125,15 @@ for s in dont_compute_transport
     end
 end
 
-for s in dont_compute_either_chem_or_transport
-    push!(no_chem_species, s)
-    push!(no_transport_species, s)
-end
 
 # Chemistry and transport participants
 # -------------------------------------------------------------------
 if converge_which == "neutrals"
-    append!(no_chem_species, union(conv_ions, N_neutrals)) # This is because the N chemistry is intimiately tied up with the ions.
-    append!(no_transport_species, union(conv_ions, N_neutrals, short_lived_species))
+    append!(no_chem_species, union(conv_ions[planet], N_neutrals)) # This is because the N chemistry is intimiately tied up with the ions.
+    append!(no_transport_species, union(conv_ions[planet], N_neutrals, short_lived_species))
 elseif converge_which == "ions"
-    append!(no_chem_species, setdiff(conv_neutrals, N_neutrals))
-    append!(no_transport_species, setdiff(conv_neutrals, N_neutrals))
+    append!(no_chem_species, setdiff(conv_neutrals[planet], N_neutrals))
+    append!(no_transport_species, setdiff(conv_neutrals[planet], N_neutrals))
 elseif converge_which == "both"
     append!(no_transport_species, short_lived_species)
 end