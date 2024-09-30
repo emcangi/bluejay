emcangi/bluejay

commit c2957453a57849f3e4a9d3b716a2858399290c0e

- Correct docstrings, clarify and fix various file headers. - Alphabetize dependencies in Photochemistry.jl EXCEPT PyPlot and PyCall which have to be called in that order because of bugs within them. - Add PLOT_STYLES.jl to Photochemistry.jl. - Fix a bug where get_deuterated is not callable from PLOT_STYLES because PLOT_STYLES needs to be loaded for the Photochemistry module to work - Add ylims argument to plot_atm calls in converge_new_file.jl.

PLOT_STYLES.jl
@@ -1,14 +1,13 @@
 ################################################################################
 # PLOT_STYLES.jl
-# TYPE: (1) Model files - required
 # DESCRIPTION: Sets some style items for plotting. This is a separate file from 
 # MODEL_SETUP.jl because this file also has to be frequently loaded separately 
 # in analysis notebooks.
 # 
 # Eryn Cangi
 # Created April 2024
-# Last edited: April 2024
-# Currently tested for Julia: 1.85
+# Last edited: May 2024
+# Currently tested for Julia: 1.8.5
 ################################################################################
 
 # ***************************************************************************************************** #
@@ -66,10 +65,16 @@ const speciescolor = Dict( # PRIMARY NEUTRALS + IONS
                     :N2Hpl=>"#611115",:N2Dpl=>"#611115", 
                     );
 
+# NOTE: Some code is repeated here below, also occurring in get_deuterated, to figure out which species are deuterated. 
+# This is because this file needs to be called by Photochemistry.jl, and I couldn't bear to not have a plot style variable
+# somewhere other than PLOT_STYLES.jl. --Eryn
+# TODO: This needs to be modified if you add new deuterated species! But, it's only plot styles, so I figured it was ok to 
+# be a little lax about this.
 known_species = keys(speciescolor)
+Dspc = [s for s in setdiff(known_species, [:Nup2D, :O1D]) if occursin('D', string(s))]
 
 # D group will have dashed lines; neutrals, solid (default)
-const speciesstyle = Dict(vcat([s=>"--" for s in setdiff(get_deuterated(known_species), [:HD2pl])], [:HD2pl=>":", :Nup2D=>"-."]) )
+const speciesstyle = Dict(vcat([s=>"--" for s in Dspc], [:HD2pl=>":", :Nup2D=>"-."]) )
 
 
 const medgray = "#444444"

INPUT_PARAMETERS.jl
@@ -1,12 +1,11 @@
 ################################################################################
 # INPUT_PARAMETERS.jl
-# TYPE: (1) Model files - required
-# DESCRIPTION: Variables to be modified for each simulation run. 
+# DESCRIPTION: Variables to be modified by the user for each simulation run. 
 # 
 # Eryn Cangi
 # Created April 2024
-# Last edited: 2024
-# Currently tested for Julia: 1.85
+# Last edited: May 2024
+# Currently tested for Julia: 1.8.5
 ################################################################################
 
 # Set the planet 

MODEL_SETUP.jl
@@ -1,14 +1,13 @@
 ################################################################################
-# PARAMETERS.jl
-# TYPE: (1) Model files - required
+# MODEL_SETUP.jl
 # DESCRIPTION: Sets up variables, constants, etc. that typically don't require
 # checking by the user before each run but change depending on the type of 
 # model being run.
 # 
 # Eryn Cangi
-# Created December 2019
-# Last edited: April 2024
-# Currently tested for Julia: 1.85
+# Created April 2024
+# Last edited: May 2024
+# Currently tested for Julia: 1.8.5
 ################################################################################
 
 using DataFrames

Photochemistry/src/Core.jl
@@ -2,13 +2,15 @@
 #                                                                              #
 #                             PHOTOCHEMICAL CORE                               #
 #                                                                              #
-# Everything necessary to run the model should be in this file with a few      #
-# exceptions:
+# This file forms the logical core of the photochemical model. Some things     #
+# not included in this file are:                                               #
 # 1. Anything related to file input/output/writing is in FileIO.jl.            #
 # 2. Everything related to photochemical cross sections is in Crosssections.jl.#
 # 3. Everything related to building the chemical network object from the       #
-#    provided spreadsheet is in ReactionNetwork.jl.
-# 
+#    provided spreadsheet is in ReactionNetwork.jl.                            #
+# 4. Really small functions necessary to the model but that are not as         #
+#    "complicated" as these core functions are in BasicUtilities.jl.           #
+#                                                                              #
 # **************************************************************************** #
 
 # Special Exception for this model.

Photochemistry/src/Photochemistry.jl
@@ -1,21 +1,23 @@
 __precompile__()
 
 module Photochemistry
-using Interpolations
-using PyPlot
-using PyCall
-using HDF5, JLD
-using LaTeXStrings
+
+using DataFrames
 using DelimitedFiles
-using SparseArrays
+using GeneralizedGenerated
+using HDF5, JLD
+using IncompleteLU 
+using Interpolations
+using LaTeXStrings
 using LinearAlgebra
 using PlotUtils
-using GeneralizedGenerated
-using DataFrames
-using XLSX
-using Random
 using Printf
-using IncompleteLU 
+using PyPlot # Do not change the order of PyPlot and PyCall, it causes problems.
+using PyCall
+using Random
+using SparseArrays
+using XLSX
+
 
 export 
 
@@ -216,6 +218,7 @@ prum_to_ppm
 code_loc = "$(@__DIR__)/../../"
 # println("Photochemistry.jl code_loc = $code_loc")
 include(code_loc*"CONSTANTS.jl")
+include(code_loc*"PLOT_STYLES.jl")
 
 include("AnalyzeChemAndTransport.jl")                                                                                
 include("BasicUtilities.jl")

converge_new_file.jl
@@ -456,7 +456,7 @@ function record_atmospheric_state(t, n, actively_solved, E_prof; opt="", globvar
     
     # write out the current atmospheric state to a file and plot it
     atm_snapshot = merge(external_storage, unflatten_atm(n, actively_solved; GV.num_layers))
-    plot_atm(atm_snapshot, results_dir*sim_folder_name*"/atm_peek_$(plotnum)$(opt).png", abs_tol_for_plot, E_prof; t="$(round(t, digits=rounding_digits))", globvars...)
+    plot_atm(atm_snapshot, results_dir*sim_folder_name*"/atm_peek_$(plotnum)$(opt).png", abs_tol_for_plot, E_prof; ylims=[zmin/1e5, zmax/1e5], t="$(round(t, digits=rounding_digits))", globvars...)
     write_atmosphere(atm_snapshot, results_dir*sim_folder_name*"/atm_state_$(lpad(plotnum,2,"0"))$(opt).h5"; t=round(t, digits=rounding_digits), globvars...)
 
     # Turn this on if you'd like to take a peek at the Jrates
@@ -1522,7 +1522,7 @@ end
     
 # Plot initial atmosphere condition  ===========================================
 println("$(Dates.format(now(), "(HH:MM:SS)")) Plotting the initial condition")
-plot_atm(n_current, results_dir*sim_folder_name*"/initial_atmosphere.png", abs_tol_for_plot, E; # initial electron profile
+plot_atm(n_current, results_dir*sim_folder_name*"/initial_atmosphere.png", abs_tol_for_plot, E; ylims=[zmin/1e5, zmax/1e5],
          t="initial state", neutral_species, ion_species, plot_grid, speciescolor, speciesstyle, zmax, hrshortcode, rshortcode,
          monospace_choice, sansserif_choice) 
 
@@ -1705,7 +1705,7 @@ elseif problem_type == "Gear"
     # Plot the final atmospheric state
     println("Plotting final atmosphere, writing out state")
     final_E_profile = electron_density(atm_soln; e_profile_type, non_bdy_layers, ion_species)   
-    plot_atm(atm_soln, results_dir*sim_folder_name*"/final_atmosphere.png", abs_tol_for_plot, final_E_profile; 
+    plot_atm(atm_soln, results_dir*sim_folder_name*"/final_atmosphere.png", abs_tol_for_plot, final_E_profile; ylims=[zmin/1e5, zmax/1e5],
              t="final converged state, total time = $(sim_time)", neutral_species, ion_species, plot_grid, speciescolor, speciesstyle, zmax, hrshortcode, rshortcode,
              monospace_choice, sansserif_choice)
 

commit a2057e9a05a2ae134ad356db24ade5d20ed54026

Fix an error introduced in c2957453a57849f3e4a9d3b716a2858399290c0e where neutrals are left out of the speciesstyle dict.

PLOT_STYLES.jl
@@ -76,7 +76,7 @@ known_species = keys(speciescolor)
 Dspc = [s for s in setdiff(known_species, [:Nup2D, :O1D]) if occursin('D', string(s))]
 
 # D group will have dashed lines; neutrals, solid (default)
-const speciesstyle = Dict(vcat([s=>"--" for s in Dspc], [:HD2pl=>":", :Nup2D=>"-."]) )
+const speciesstyle = Dict(vcat([s=>"--" for s in Dspc], [s=>"-" for s in setdiff(keys(speciescolor), Dspc)], [:HD2pl=>":", :Nup2D=>"-."]) )
 
 
 const medgray = "#444444"