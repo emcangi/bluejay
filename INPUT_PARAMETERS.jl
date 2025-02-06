58f7ea081ee5c7a001bbeb7ace8a550cbe2f7e36
Simon Correra <simon.correra@uconn.edu>
2025-02-06 14:28:59 -0500

    inputparameter with the new Cl and S species, and i reorganized the species lists

--- INPUT_PARAMETERS.jl
+++ INPUT_PARAMETERS.jl
@@ -20,7 +20,7 @@ const planet = "Venus"
 # Input and output files, directory
 # =======================================================================================================
 const results_dir = code_dir*"../Results_$(planet)/"
-const initial_atm_file = "$(planet)-Inputs/made DpHCltoHDpCl mass scale.h5"  # File to use to initialize the atmosphere.
+const initial_atm_file = "$(planet)-Inputs/added type 7 reaction and ClCO3.h5"  # File to use to initialize the atmosphere.
     # OPTIONS: 
     # INITIAL_GUESS_MARS.h5 --> Basic Mars starting file.
     # INITIAL_GUESS_MARS_bxz4YnHk.h5 --> A Mars atmosphere that includes N2O, NO2, and their ions;
@@ -129,15 +129,41 @@ const conv_neutrals = Dict("Mars"=>[:Ar, :C, :CO, :CO2, # Argon and carbon speci
                                     :H2O2, :HDO2, :HOCO, :DOCO, 
                                     :N, :N2, :NO, :Nup2D, :N2O, :NO2, # Nitrogen species
                                     :O, :O1D, :O2, :O3, :OH, :OD], # Oxygen species
-                           "Venus"=>[:Ar, :C, :CO, :CO2, 
-                                     :Cl, :ClO, :ClCO, :HCl, :DCl,  # Chlorine species
-                                     :H, :D, :H2, :HD, :H2O, :HDO,  # H and D species
-                                     :HCO, :DCO, :HO2, :DO2,        
+                           "Venus"=>[
+                                     # Ar
+                                     :Ar,
+        
+                                     # C species
+                                     :C, :CO2, :CO,
+
+                                     # Chlorine species
+                                     :Cl, :Cl2, :HCl, :DCl, :ClNO, 
+                                     :ClO, :COCl2, :ClCO, :ClO2, :ClCO3,           
+                                                                 
+                                     # H and D species
+                                     :H, :D, :H2, :HD, :H2O, :HDO,
+                                     :HCO, :DCO, 
+                                     :HO2, :DO2,        
                                      :H2O2, :HDO2, :HOCO, :DOCO, 
+
+                                     # N species
                                      :N, :N2, :NO, :Nup2D, :N2O, :NO2,
-                                     :O, :O1D, :O2, :O3, :OH, :OD,
-                                     :S, :SO, :SO2, :SO3, :H2SO4, :HDSO4] # Sulfur species
-                           ); 
+                                     :HO2NO2, :DO2NO2,
+
+                                     # O species
+                                     :O2, :O, :O1D,  
+                                     :O3, :OH, :OD,
+
+                                     # Sulfur species
+                                     :S, :S2, :S3,
+                                     :SO, :SO2, :SO3, :H2SO4, :HDSO4, :HSO3, :DSO3,
+                                     :SNO,  :S2O, :S2O2, :OCS, 
+
+                                     # Cl and S species
+                                     :SCl, :SCl2, :S2Cl2, :ClS2,
+                                     :OSCl, :ClSO2, :SO2Cl2,
+                           ]); 
+
 
 const conv_ions = Dict("Mars"=>[:Arpl, :ArHpl, :ArDpl, 
                                 :Cpl, :CHpl, :COpl, :CO2pl, 
@@ -147,15 +173,22 @@ const conv_ions = Dict("Mars"=>[:Arpl, :ArHpl, :ArDpl,
                                 :HO2pl, :HCOpl, :HCO2pl, :HOCpl, :HNOpl,   
                                 :Npl, :NHpl, :N2pl, :N2Hpl, :N2Dpl, :NOpl, :N2Opl, :NO2pl,
                                 :Opl, :O2pl, :OHpl, :ODpl],
-                       "Venus"=>[:Arpl, :ArHpl, :ArDpl, 
-                                :Cpl, :CHpl, :COpl, :CO2pl, 
-                                :Dpl, :DCOpl, :DOCpl, :DCO2pl, 
-                                :Hpl,  :H2pl, :HDpl, :H3pl, :H2Dpl, 
-                                :H2Opl, :HDOpl, :H3Opl, :H2DOpl, 
-                                :HO2pl, :HCOpl, :HCO2pl, :HOCpl, :HNOpl,   
-                                :Npl, :NHpl, :N2pl, :N2Hpl, :N2Dpl, :NOpl, :N2Opl, :NO2pl,
-                                :Opl, :O2pl, :OHpl, :ODpl]
-                      );
+                       "Venus"=>[:CO2pl, :H2Opl, :HDOpl, :COpl, :O2pl, :Hpl, :Dpl, :Opl, :H2pl, :HDpl,
+        
+                                :ArHpl, :ArDpl, :Arpl,
+                                :N2pl,
+                                :Cpl, 
+                                :CHpl, 
+                                :DCOpl, :DOCpl, :DCO2pl, 
+                                :H3pl, :H2Dpl, 
+                                :H3Opl, :H2DOpl, 
+                                :HO2pl, 
+                                :HCOpl, :HCO2pl, :HOCpl, :HNOpl,   
+                                :Npl, :NHpl, :N2Hpl, :N2Dpl, 
+                                :NOpl, 
+                                :N2Opl, :NO2pl,
+                                :OHpl, :ODpl
+                      ]);
 
 # More specific settings for controling the modeling of species
 # -------------------------------------------------------------------
