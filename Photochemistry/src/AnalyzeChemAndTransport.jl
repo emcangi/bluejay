cf9fc6dbec668889967b9ff3b3ddabb4d2c06383
Eryn Cangi <emcangi@protonmail.com>
2026-05-26 17:29:43 -0600

    replace a direct call on a reaction rate coefficient expression with the function call that was designed to evaluate it

--- Photochemistry/src/AnalyzeChemAndTransport.jl
+++ Photochemistry/src/AnalyzeChemAndTransport.jl
@@ -186,7 +186,7 @@ function get_volume_rates(sp::Symbol, source_rxn::Vector{Any}, source_rxn_rc_fun
         remove_me = remove_sp_density==true ? sp : nothing
         density_prod = reactant_density_product(atmdict, source_rxn[1]; removed_sp=remove_me, globvars...)
         thisrate = typeof(source_rxn[3]) != Expr ? :($source_rxn[3] + 0) : source_rxn[3]
-        rate_coef = source_rxn_rc_func(GV.Tn, GV.Ti, GV.Te, Mtot)
+        rate_coef = eval_rate_coef(atmdict, thisrate; globvars...) # source_rxn_rc_func(GV.Tn, GV.Ti, GV.Te, Mtot, GV.uncert_power)
 
         vol_rates = density_prod .* rate_coef # This is k * [R1] * [R2] where [] is density of a reactant. 
     end
