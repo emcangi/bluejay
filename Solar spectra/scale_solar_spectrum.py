d27359d467350f96175db70c728573089eca9d58
Eryn Cangi <emcangi@protonmail.com>
2026-06-12 17:06:19 -0600

    Modify solar spectrum creator so we can enter lists of files instead of typing into the prompt, and make a bunch all at once.

--- Solar spectra/scale_solar_spectrum.py
+++ Solar spectra/scale_solar_spectrum.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.interpolate as interp
+from copy import deepcopy
 import datetime
 from scipy.optimize import curve_fit
 
@@ -40,7 +41,8 @@ def interpolate_solar_spectrum(spec, AU, planet, show_plots=True, extrap_tail=Tr
         scale_below, scale_below: floats
                                   At wavelengths inside [scale_below, scale_above], the data are assumed
                                   to be taken in situ at the planet and to not need to be scaled. 
-                                  OUTSIDE this range, data will be scaled by 1/(AU^2). 
+                                  OUTSIDE this range, data will be scaled by 1/(AU^2), assuming an Earth-
+                                  orbit origin for the data, e.g. SORCE.
                                   e.g., EUVM data from MAVEN at Mars are taken from 0.5--189.5 nm, so when 
                                   EUVM data is included in the input file, one should set scale_below=0.5
                                   and scale_above=189.5.
@@ -116,77 +118,123 @@ def interpolate_solar_spectrum(spec, AU, planet, show_plots=True, extrap_tail=Tr
     else:
         whole_spectrum = newsolardata
     
-    #make the FINAL dataframe with ALL data for solar max;
-    # First convert from W/m^2/nm to the units below, and yes, I have checkd it multiple times lol including on 11/3/22. 
-    # multiply the irradiance by λ/hc to get photons; by 1/10000 to convert to cm^2; and 1/AU^2 to convert to planet orbit.
-    # whole_spectrum["photon flux (phot/s/cm^2/nm)"] = whole_spectrum[col[1]] * ((whole_spectrum[col[0]] * 10**(-9))/(h*c)) * 1/(AU**2) * (1/10000)
-    
-    whole_spectrum["photon flux (phot/s/cm^2/nm)"] = np.asarray(list(map(convert_W_m2_to_photons_per_s_cm2, whole_spectrum[col[1]], whole_spectrum[col[0]])))
-
-    # Now scale to the right AU.
-    # You can select whether to only scale above a certain wavelength if you 
-    # are using MAVEN EUVM data for the short wavelengths!
-    if scale_above==0:
-        i = 0
-    else:
-        i = find_nearest(whole_spectrum[col[0]], scale_above)
-    
-    whole_spectrum.loc[i:, "photon flux (phot/s/cm^2/nm)"] = whole_spectrum.loc[i:, "photon flux (phot/s/cm^2/nm)"] * (1**2)/(AU**2) 
-
-    if show_plots:
-        plt.figure(figsize=(10,5))
-        plt.title("Finalized spectrum, in photon flux, with interpolation and extrapolation")
-        plt.plot(whole_spectrum[col[0]], whole_spectrum["photon flux (phot/s/cm^2/nm)"])
-        plt.xlabel("Wavelength (nm)")
-        plt.ylabel("Photons (ph cm^-2 s^-1)")
-        plt.savefig(f"final_spectrum_{planet}_{desctag}.png", bbox_inches="tight")
-        plt.show()
-        print()
     
     return whole_spectrum
 
 # MAIN =======================================================================================================================
 # Get user input -------------------------------------------------------------------------------------------------------------
-planet_name = input("Enter planet name: ")
-solarfile = input("Enter the file with solar spectrum data in W/m^2/nm: ")
-theAU = float(input("Enter AU at which you'd like the output: "))
-descriptive_tag = input("Enter a descriptive tag for this spectrum (please use underscores): ")
-in_situ_data = input(f"Is any of the data taken by a mission that orbits {planet_name}? (Please enter True or False): ")
+show_plots=False
+planet_name = "Mars" # input("Enter planet name: ")
+solarfiles = ["solarmin_20081029_SORCE_and_TIMED.dat", 
+             "solarmean_2004-02-07_SORCE_and_TIMED.dat", 
+             "solarmax_20150604_and_20020322_SORCE_and_TIMED.dat"] # input("Enter the file with solar spectrum data (taken in Earth orbit) in W/m^2/nm: ")
+theAU = [1.624, 1.656, 1.537, 1.411, 1.387, 1.483] # float(input("Enter AU at which you'd like the output: "))
 
-if in_situ_data == "True": 
-    scale_below = float(input("Please enter the minimum wavelength (nm) of your in-situ data: "))
-    scale_above = float(input("Please enter the maximum wavelength (nm) of your in-situ data: "))
-    print(f"In situ data used between {scale_below} and {scale_above} nm. Those data will not be scaled by AU. In all other regions, data will be scaled by (1/{theAU})^2.")
-else:
-    scale_below = 0
-    scale_above = 0
-    print(f"Scaling all data to a distance of {theAU} AU, using SORCE and TIMED (Earth-based) dataonly.")
+use_data = True # input(f"Should we apply data from another instrument at {planet_name}? (Please enter True or False): ")
 
-# Count header rows in the source file, hopefully they start with # or this will silently fail
-hdrcount = 0
-with open(solarfile) as f: 
-    for line in f:
-        if line.startswith("#"):
-            hdrcount += 1
+datafiles = ["euvm_60deg_bins/mvn_euv_l3_Ls30_2023-2-27.csv",
+            "euvm_60deg_bins/mvn_euv_l3_Ls90_2023-7-13.csv",
+            "euvm_60deg_bins/mvn_euv_l3_Ls150_2023-11-18.csv",
+            "euvm_60deg_bins/mvn_euv_l3_Ls210_2024-3-4.csv",
+            "euvm_60deg_bins/mvn_euv_l3_Ls270_2022-7-22.csv",
+            "euvm_60deg_bins/mvn_euv_l3_Ls330_2022-10-31.csv",] # input(f"Please enter the filename with data from {planet_name}: ")
 
-# Load data, interpolate, and scale -----------------------------------------------------------------------------------------
-print(f"loading {solarfile}")
-solarspec = np.loadtxt(solarfile, skiprows=hdrcount)
-solarspec_df = pd.DataFrame(solarspec, columns=["wavelength (nm)", "irradiance (W/m^2/nm)"])
-solarspec_df_tidy = interpolate_solar_spectrum(solarspec_df, theAU, planet_name, show_plots=True, scale_below=scale_below, scale_above=scale_above, desctag=descriptive_tag)
+scale_below = 0
+scale_above = 0
+datspec = None
 
-# Write out the result to a file --------------------------------------------------------------------------------------------
+if use_data == True: 
+    tempdat = pd.read_csv(datafiles[0], delimiter=",", header=0, usecols=["wavelength (nm)", "irradiance (W/m^2)"])
+    scale_below = tempdat["wavelength (nm)"][0]
+    scale_above = tempdat["wavelength (nm)"][len(tempdat)-1]
+    print(f"In situ data used between {scale_below} and {scale_above} nm. Those data will not be scaled by AU. In all other regions, data will be scaled by the specified AU.")
 
-# Build a custom header and writeout the file, with custom header comments before the column names
-header = [f"# AU: {theAU}"+'\\n',
-          f"# Source data: {solarfile}"+'\\n',
-          f"# File creation date: {datetime.datetime.now().strftime('%B %d, %Y')}"+'\\n\\n']
-          
-outputfile = f"{planet_name}solarphotonflux_{descriptive_tag}.dat"
+descriptive_tag = ["mean-Ls0-60", 
+                   "mean-Ls60-120", 
+                   "mean-Ls120-180", 
+                   "mean-Ls180-240", 
+                   "mean-Ls240-300", 
+                   "mean-Ls310-360"] # input("Enter a descriptive tag for this spectrum (please use underscores): ")
 
-with open(outputfile, "w") as f:  
-    for line in header:
-        f.write(line)
-    f.write("# ")
-    solarspec_df_tidy.to_csv(f, sep='\t', float_format="%.2f", columns=["wavelength (nm)", "photon flux (phot/s/cm^2/nm)"], index=False)
+for solarfile in solarfiles:
+    # Count header rows in the source file, hopefully they start with # or this will silently fail
+    hdrcount = 0
+    with open(solarfile) as f: 
+        for line in f:
+            if line.startswith("#"):
+                hdrcount += 1
+
+
+    # Load data, interpolate, and scale -----------------------------------------------------------------------------------------
+    print(f"loading {solarfile}")
+    solarspec = np.loadtxt(solarfile, skiprows=hdrcount)
+    solarspec_df = pd.DataFrame(solarspec, columns=["wavelength (nm)", "irradiance (W/m^2/nm)"])
+
+    for mdf, AU, desc in zip(datafiles, theAU, descriptive_tag):
+
+        # Open mars data file
+        datspec = pd.read_csv(mdf, delimiter=",", header=0, usecols=["wavelength (nm)", "irradiance (W/m^2)"])
+
+        # make a copy
+        solarspec_thisiter = deepcopy(solarspec_df)
+
+        # Apply data from in situ
+        paste_over_these = solarspec_thisiter.index[solarspec_thisiter["wavelength (nm)"].isin(datspec["wavelength (nm)"])].tolist()
+        solarspec_thisiter.loc[paste_over_these, "irradiance (W/m^2/nm)"] = datspec["irradiance (W/m^2)"]
+
+        # Interpolate
+        interp_spec = interpolate_solar_spectrum(solarspec_thisiter, AU, planet_name, show_plots=show_plots, 
+                                                 desctag=desc)
+
+        # Convert from W/m^2/nm to the units below, and yes, I have checked it multiple times lol including on 11/3/22. 
+        # multiply the irradiance by λ/hc to get photons; by 1/10000 to convert to cm^2; and 1/AU^2 to convert to planet orbit.
+        # interp_spec["photon flux (phot/s/cm^2/nm)"] = interp_spec[col[1]] * ((interp_spec[col[0]] * 10**(-9))/(h*c)) * 1/(AU**2) * (1/10000)
+        interp_spec["photon flux (phot/s/cm^2/nm)"] = np.asarray(list(map(convert_W_m2_to_photons_per_s_cm2, interp_spec[interp_spec.columns[1]], interp_spec[interp_spec.columns[0]])))
+
+        # Now scale to the right AU, avoiding regions where we grabbed data from the planet iteself.
+        # Above some value
+        if scale_above==0:
+            i = 0
+        else:
+            i = find_nearest(interp_spec[interp_spec.columns[0]], scale_above)
+        interp_spec.loc[i:, "photon flux (phot/s/cm^2/nm)"] = interp_spec.loc[i:, "photon flux (phot/s/cm^2/nm)"] * (1**2)/(AU**2)
+
+        # Below some value
+        if scale_below==0:
+            i = 0
+        else:
+            i = find_nearest(interp_spec[interp_spec.columns[0]], scale_above)
+        interp_spec.loc[:i, "photon flux (phot/s/cm^2/nm)"] = interp_spec.loc[:i, "photon flux (phot/s/cm^2/nm)"] * (1**2)/(AU**2)
+
+
+        if show_plots:
+            plt.figure(figsize=(10,5))
+            plt.title("Finalized spectrum, in photon flux, with interpolation and extrapolation")
+            plt.plot(interp_spec[interp_spec.columns[0]], interp_spec["photon flux (phot/s/cm^2/nm)"])
+            plt.xlabel("Wavelength (nm)")
+            plt.ylabel("Photons (ph cm^-2 s^-1)")
+            plt.savefig(f"final_spectrum_{planet_name}_{descriptive_tag}.png", bbox_inches="tight")
+            plt.show()
+            print()
+
+
+        # Write out the result to a file --------------------------------------------------------------------------------------------
+
+        # Build a custom header and writeout the file, with custom header comments before the column names
+        sourceinfo = f"# Solar source: {solarfile}"
+        if use_data==True:
+            sourceinfo += f", data at {planet_name} from {scale_below} to {scale_above}: {mdf}"
+        sourceinfo += '\\n'
+
+        header = [f"# AU: {AU}"+'\\n',
+                  sourceinfo,
+                  f"# File creation date: {datetime.datetime.now().strftime('%B %d, %Y')}"+'\\n\\n']
+                  
+        outputfile = f"{planet_name}solarphotonflux_{desc}.dat"
+
+        with open(outputfile, "w") as f:  
+            for line in header:
+                f.write(line)
+            f.write("# ")
+            interp_spec.to_csv(f, sep='\t', float_format="%.2f", columns=["wavelength (nm)", "photon flux (phot/s/cm^2/nm)"], index=False)
 
